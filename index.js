617071673505cae29752105ff01583d5c042ba1f
Mariia <m.goncharenkoo@icloud.com>
2022-09-24 08:26:34 -0400

    commit tree 542fda83e79671eb0d4fe0aa2da253c95dc22ce0

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+const events = ["birthday"]
+
+function writeCards(names, events) {
+    const cards = []
+    for (let num = 0; num < names.length; num++) {
+        cards.push(`Thank you, ${names[num]}, for the wonderful ${events} gift!`);
+    }
+    return cards;
+}
+
+function countDown(n) {
+    let countup = n;
+while (countup >= 0) {
+  console.log(countup--);
+}
+}
